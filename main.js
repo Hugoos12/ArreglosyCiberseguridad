15d04f0a97e86c1ef5e909381382d123af035bc0
ArreglosCiberseguridad <148265243+Hugoos12@users.noreply.github.com>
2024-01-13 14:40:38 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
+const inputs = document.querySelectorAll(".form__input");
 
-const inputs = document.querySelectorAll(".form__input")
-
-
-function addfocus(){
-    let parent = this.parentNode.parentNode
-    parent.classList.add("focus")
+function addFocus() {
+    let parent = this.parentNode.parentNode;
+    parent.classList.add("focus");
 }
 
-
-function remfocus(){
-    let parent = this.parentNode.parentNode
-    if(this.value == ""){
-        parent.classList.remove("focus")
+function removeFocus() {
+    let parent = this.parentNode.parentNode;
+    if (this.value === "") {
+        parent.classList.remove("focus");
     }
 }
 
-inputs.forEach(input=>{
-    input.addEventListener("focus",addfocus)
-    input.addEventListener("blur",remfocus)
-})
+inputs.forEach(input => {
+    input.addEventListener("focus", addFocus);
+    input.addEventListener("blur", removeFocus);
+});
