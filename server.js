b827d919c1ed64ea3a86da4e0a774c7d913ce5ae
ArreglosCiberseguridad <148265243+Hugoos12@users.noreply.github.com>
2024-01-17 18:11:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -16,3 +16,31 @@ app.use(express.json());
 app.listen(PORT, () => {
   console.log(`Servidor iniciado en http://localhost:${27017}`);
 });
+
+const Usuario = require('./models/Usuario');
+
+app.post('/registro', async (req, res) => {
+  try {
+    const usuario = await Usuario.create(req.body);
+    res.status(201).json(usuario);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error en el servidor');
+  }
+});
+
+app.post('/inicio-sesion', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const usuario = await Usuario.findOne({ username, password });
+    if (usuario) {
+      res.status(200).json(usuario);
+    } else {
+      res.status(401).send('Credenciales incorrectas');
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error en el servidor');
+  }
+});
